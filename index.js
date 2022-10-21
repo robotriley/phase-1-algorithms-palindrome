7b21fa5088f31364e60cfe9b229031f4104fc9a3
riley soloner <rileysoloner@gmail.com>
2022-10-21 12:19:01 -0400

    commit tree a4ab231b910328ec3f9b53d802cec8370b82c450

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// 1. Rewrite the Problem in Your Own Words
+        // I'm writing a function that takes in one argument, a string.
+        // The function will return true if the string is a palindrome, false if it's not a palindrome.
+        // A palindrome is a word or phrase that is spelled the same way read forwards or backwards.
+// 2. Write Your Own Test Cases
+        // I expect calling isPalindrome with the string "abba" will return true
+
+        // I expect calling isPalindrome with the string "granola" will return false
+// 3. Pseudocode
+      // define function isPalindrome(word)
+      // define three functions inside isPalindrome:
+      // 1. splitString(). This creates an array of the individual characters in the string argument
+      // 2. reverseArray(). This will reverse the order of the items in the new array
+      // 3. joinArray(). This will join the array's items into a string
+      // 4. if joinArray is exactly equal to word, return true.
+// 4. Code
+// 5. Make It Clean and Readable
+// 6. Optimize
+
+const isPalindrome = (word) => {
+  let splitString = word.split("")
+  let reverseArray = splitString.reverse()
+  let joinArray = reverseArray.join("")
+   if (joinArray === word) {
+    return true 
+   }
+   else if (joinArray != word) {
+    return false
+   }
 }
 
 /* 
